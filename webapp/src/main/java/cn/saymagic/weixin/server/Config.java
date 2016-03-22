eb26f04c1a279baaf7154be0478c06dbabe180a5
zhaoj <openbook114@sohu.com>
2016-03-22 12:31:40 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "python";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "29ab2304e4755e9a2e746e27642913be"; 
 	
 }
